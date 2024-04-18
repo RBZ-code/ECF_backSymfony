79a71fe9d660c3df5d75854dada2dbf47d6e382c
Arègue R H <arah@hotmail.fr>
2024-04-18 16:42:23 +0200

    le commit de base

---
+++ migrations/Version20240418143150.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240418143150 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE subscription_type (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE subscription ADD type_id INT NOT NULL, DROP type');
+        $this->addSql('ALTER TABLE subscription ADD CONSTRAINT FK_A3C664D3C54C8C93 FOREIGN KEY (type_id) REFERENCES subscription_type (id)');
+        $this->addSql('CREATE INDEX IDX_A3C664D3C54C8C93 ON subscription (type_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE subscription DROP FOREIGN KEY FK_A3C664D3C54C8C93');
+        $this->addSql('DROP TABLE subscription_type');
+        $this->addSql('DROP INDEX IDX_A3C664D3C54C8C93 ON subscription');
+        $this->addSql('ALTER TABLE subscription ADD type VARCHAR(255) NOT NULL, DROP type_id');
+    }
+}
