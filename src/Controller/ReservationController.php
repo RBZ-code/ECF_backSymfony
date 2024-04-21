2d00249897edf50ae4c34a4272570d0f6e85ff7a
RBZ-code <romainbezolles@hotmail.fr>
2024-04-21 18:16:38 +0200

    restriction calendrier lundi/vendredi 8h/18h + page reservation Heure dynamique en fonction de la capacity de la salle

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 // Dans ReservationController.php
 
+use App\Entity\Room;
 use App\Entity\Reservation;
 use App\Form\ReservationType;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ReservationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,11 +25,11 @@ class ReservationController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Enregistrer la réservation en base de données
+            
             $entityManager->persist($reservation);
             $entityManager->flush();
 
-            // Passer l'objet complet de la réservation à la vue
+            
             return $this->render('reservation/confirmReservation.html.twig', [
                 'reservation' => $reservation,
             ]);
@@ -35,6 +37,52 @@ class ReservationController extends AbstractController
 
         return $this->render('reservation/index.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/heureReservation/{id}', name: 'heure_reservation')]
+    public function reservation(Room $room, Request $request, ReservationRepository $reservationRepository): Response
+    {
+        // Récupérer la date sélectionnée depuis la requête
+        $selectedDate = $request->query->get('selectedDate');
+        //dd($selectedDate);
+
+        
+        $currentDate = new \DateTime();
+        $currentHour = date('H');
+
+       
+        $hoursOfDay = [];
+        for ($hour = 8; $hour <= 18; $hour++) {
+            $hoursOfDay[] = sprintf('%02d:00', $hour); // Formatage de l'heure
+        }
+
+       
+        $fullHours = [];
+
+        
+        // Vérifier pour chaque heure si le total des réservations atteint la capacité de la salle
+        foreach ($hoursOfDay as $hour) {
+            
+            $selectedDateTime = new \DateTime($selectedDate . ' ' . $hour);
+
+            
+            $totalReservationsAtHour = $reservationRepository->findReservationsByRoomAndHour($room, $selectedDateTime);
+
+            
+            if ($totalReservationsAtHour >= $room->getCapacity()) {
+                $fullHours[] = $hour;
+
+            }
+        }
+        //dd( $fullHours);
+
+        return $this->render('reservation/heureReservation.html.twig', [
+            'unavailable_hours' => $fullHours,
+            'selectedDate' => $selectedDate, 
+            'room' => $room,
+            'current_hour' => $currentHour,
+            'current_date' => $currentDate
         ]);
     }
 }
--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -5,11 +5,8 @@ namespace App\Controller;
 use App\Entity\Room;
 use App\Repository\RoomRepository;
 use App\Repository\EquipmentRepository;
-use App\Repository\ReservationRepository;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RoomController extends AbstractController
@@ -34,37 +31,12 @@ class RoomController extends AbstractController
 
 
     #[Route('/reservation/{id}', name: 'app_reservation')]
-    public function test(Room $room, ReservationRepository $reservationRepository): Response
+    public function reservation(Room $room,): Response
     {
-        // Récupérer la date actuelle
-        $currentDate = new \DateTime();
-    
-        // Récupérer toutes les heures possibles dans une journée
-        $hoursOfDay = [];
-        for ($hour = 0; $hour < 24; $hour++) {
-            $hoursOfDay[] = sprintf('%02d:00', $hour); // Formatage de l'heure
-        }
-    
-        // Tableau pour stocker les heures pleines
-        $fullHours = [];
-    
-        // Vérifier pour chaque heure si le total des réservations atteint la capacité de la salle
-        foreach ($hoursOfDay as $hour) {
-            // Créer une instance de DateTime pour cette heure
-            $hourDateTime = \DateTime::createFromFormat('H:i', $hour);
-            
-            // Récupérer le nombre total de réservations pour cette salle à cette heure
-            $totalReservationsAtHour = $reservationRepository->findReservationsByRoomAndHour($room, $hourDateTime);
-            
-            // Vérifier si le nombre total de réservations pour cette heure atteint la capacité de la salle
-            if ($totalReservationsAtHour >= $room->getCapacity()) {
-                $fullHours[] = $hour;
-            }
-        }
-        //dd($fullHours);
+        //dd($room);
     
         return $this->render('room/calendrier.html.twig', [
-            'unavailable_hours' => $fullHours
+            'room' => $room,
         ]);
     }
 }
